4ac249709541c48cde1ac9782a5d3a8e35e677cc
Sandra Wazaz <sandrawazaz@gmail.com>
2020-12-23 23:20:32 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -6,6 +6,6 @@ sum = first_number + second_number
 
 difference = first_number - second_number
 
-product = first_number * second
+product = first_number * second_number
 
 quotient = first_number/second_number
